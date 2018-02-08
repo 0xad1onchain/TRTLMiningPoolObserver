10a3ee92f63104d72aa5228d1d4a7cd14de84536
Aditya Gupta <adi.gupta13@gmail.com>
2018-02-08 23:17:02 +0530

    added UI integration

--- app/src/main/java/ml/fifty9/poolmonitor/DashboardFragment.java
+++ app/src/main/java/ml/fifty9/poolmonitor/DashboardFragment.java
@@ -9,8 +9,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.List;
+
 import ml.fifty9.poolmonitor.model.Charts;
 import ml.fifty9.poolmonitor.model.Pool;
+import ml.fifty9.poolmonitor.model.Stats;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,6 +21,9 @@ import ml.fifty9.poolmonitor.model.Pool;
  */
 //TODO make the UI nice :p
 public class DashboardFragment extends Fragment {
+
+    private TextView hashRate, lastShare, paid, balance, submittedHashes;
+    List<List<Integer>> hashes;
 
     public DashboardFragment() {
         // Required empty public constructor
@@ -29,9 +35,56 @@ public class DashboardFragment extends Fragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_dashboard, container, false);
         Charts chart = ((ParentActivity)this.getActivity()).getChart();
-        TextView head = (TextView) view.findViewById(R.id.hash_rate);
-        head.setText(chart.getPayments().toString());
+        Stats stats = ((ParentActivity)this.getActivity()).getStats();
+        hashes = chart.getHashrate();
+
+        hashRate = (TextView) view.findViewById(R.id.hash_rate);
+        lastShare = (TextView) view.findViewById(R.id.last_share_text);
+        paid = (TextView) view.findViewById(R.id.paid_text);
+        balance = (TextView) view.findViewById(R.id.balance_text);
+        submittedHashes = (TextView) view.findViewById(R.id.hash_submitted_text);
+
+
+        balance.setText(convertCoin(stats.getBalance()));
+        paid.setText(convertCoin(stats.getPaid()));
+        submittedHashes.setText(stats.getHashes());
+        lastShare.setText(getDate(stats.getLastShare()));
+        hashRate.setText(getHashRate());
+
         return view;
+    }
+
+    public String getHashRate() {
+
+        int totalTime = 0;
+        int totalHashes = 0;
+
+        for (int i = 0; i < hashes.size(); i++) {
+
+            List<Integer> entry = hashes.get(i);
+            totalHashes = totalHashes + entry.get(1) * entry.get(2);
+            totalTime = totalTime + entry.get(2);
+
+        }
+
+
+        String hashRate = String.valueOf(totalHashes / totalTime) + " H/s";
+
+        return hashRate;
+
+    }
+
+    public String convertCoin(String coin) {
+        int coins = Integer.parseInt(coin);
+        coins = coins/100;
+        return String.valueOf(coins);
+    }
+
+    public String getDate(String timeStampString) {
+
+        Long timeLong = Long.parseLong(timeStampString);
+        java.util.Date time=new java.util.Date((long)timeLong*1000);
+        return time.toString();
     }
 
 
