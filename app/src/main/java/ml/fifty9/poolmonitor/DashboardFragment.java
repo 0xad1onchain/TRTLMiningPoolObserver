26b6583e2d3a719e6ab17d09560b0d27e0995dff
Aditya Gupta <adi.gupta13@gmail.com>
2018-02-08 17:55:22 +0530

    Fixed Data Passing

--- app/src/main/java/ml/fifty9/poolmonitor/DashboardFragment.java
+++ app/src/main/java/ml/fifty9/poolmonitor/DashboardFragment.java
@@ -7,14 +7,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-import io.reactivex.disposables.CompositeDisposable;
+import ml.fifty9.poolmonitor.model.Charts;
 import ml.fifty9.poolmonitor.model.Pool;
-import ml.fifty9.poolmonitor.util.RetrofitAPI;
-import ml.fifty9.poolmonitor.util.RetrofitService;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,7 +18,6 @@ import retrofit2.Response;
  */
 //TODO make the UI nice :p
 public class DashboardFragment extends Fragment {
-    private RetrofitAPI retrofitAPI;
 
     public DashboardFragment() {
         // Required empty public constructor
@@ -33,30 +28,11 @@ public class DashboardFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_dashboard, container, false);
-        retrofitAPI = RetrofitService.getAPI();
-        callAPI();
+        Charts chart = ((ParentActivity)this.getActivity()).getChart();
+        TextView head = (TextView) view.findViewById(R.id.main_text);
+        head.setText(chart.getPayments().toString());
         return view;
     }
 
-    private void callAPI() {
-        retrofitAPI.queryDashboardStats("TRTLux32vos9TtGNRQTux4WLFQTBX2ii3RMzGsrbN2dKatwucquqQRmUUzXkuiNyEA5NCqPUiP3SeSeJVT9bpW3SGyWKmRcoT3Z")
-                    .enqueue(new Callback<Pool>() {
-                        @Override
-                        public void onResponse(Call<Pool> call, Response<Pool> response) {
-                            /**
-                             * Get response from the API
-                             * use response.body.getCharts() for various payments and hashrates
-                             * use response.body.getStats() to get details like hashes,lastShare, balance, paid
-                             */
-                            Log.d("Response from API",
-                                    response.body().getCharts().getPayments().get(0).get(0).toString());
-                        }
-
-                        @Override
-                        public void onFailure(Call<Pool> call, Throwable t) {
-                            Log.d("Error",t.getMessage());
-                        }
-        });
-    }
 
 }
--- app/src/main/java/ml/fifty9/poolmonitor/ParentActivity.java
+++ app/src/main/java/ml/fifty9/poolmonitor/ParentActivity.java
@@ -8,28 +8,41 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import ml.fifty9.poolmonitor.model.Charts;
+import ml.fifty9.poolmonitor.model.Pool;
+import ml.fifty9.poolmonitor.model.Stats;
+import ml.fifty9.poolmonitor.util.RetrofitAPI;
+import ml.fifty9.poolmonitor.util.RetrofitService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ParentActivity extends AppCompatActivity {
     private SectionPagerAdapter mSectionPagerAdapter;
     private ViewPager mViewPager;
     private Toolbar toolbar;
+    private RetrofitAPI retrofitAPI;
+    private Charts chartObj;
+    private Stats statObj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent);
-
-        mSectionPagerAdapter = new SectionPagerAdapter(getSupportFragmentManager());
-        mViewPager = findViewById(R.id.container);
-        toolbar = findViewById(R.id.toolbar);
-
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Pool Monitor");
-
-        mViewPager.setAdapter(mSectionPagerAdapter);
-        TabLayout tabLayout = findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
+        retrofitAPI = RetrofitService.getAPI();
+        callAPI();
     }
+
+    public Charts getChart() {
+        return this.chartObj;
+    }
+
+    public Stats getStats() {
+        return this.statObj;
+    }
+
 
     private class SectionPagerAdapter extends FragmentPagerAdapter {
 
@@ -70,4 +83,52 @@ public class ParentActivity extends AppCompatActivity {
         }
 
     }
+
+    private void callAPI() {
+        retrofitAPI.queryDashboardStats("TRTLux32vos9TtGNRQTux4WLFQTBX2ii3RMzGsrbN2dKatwucquqQRmUUzXkuiNyEA5NCqPUiP3SeSeJVT9bpW3SGyWKmRcoT3Z")
+                .enqueue(new Callback<Pool>() {
+                    @Override
+                    public void onResponse(Call<Pool> call, Response<Pool> response) {
+                        /**
+                         * Get response from the API
+                         * use response.body.getCharts() for various payments and hashrates
+                         * use response.body.getStats() to get details like hashes,lastShare, balance, paid
+                         */
+                        Log.d("Response from API",
+                                response.body().getCharts().getPayments().get(0).get(0).toString());
+
+                        setAPIObjects(response);
+                        inflateTabs();
+                    }
+
+                    @Override
+                    public void onFailure(Call<Pool> call, Throwable t) {
+                        Log.d("Error",t.getMessage());
+                    }
+                });
+    }
+
+    private void setAPIObjects(Response<Pool> response) {
+
+        chartObj = response.body().getCharts();
+        statObj = response.body().getStats();
+
+    }
+
+    private void inflateTabs() {
+
+        mSectionPagerAdapter = new SectionPagerAdapter(getSupportFragmentManager());
+        mViewPager = findViewById(R.id.container);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Pool Monitor");
+
+        mViewPager.setAdapter(mSectionPagerAdapter);
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+    }
+
 }
+
