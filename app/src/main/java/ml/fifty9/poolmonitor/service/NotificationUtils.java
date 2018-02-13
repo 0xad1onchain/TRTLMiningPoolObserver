51cf06115a7a6905cfb782c08c8caa06131d9379
Aditya Gupta <adi.gupta13@gmail.com>
2018-02-13 18:16:52 +0530

    Changed notification priority

--- app/src/main/java/ml/fifty9/poolmonitor/service/NotificationUtils.java
+++ app/src/main/java/ml/fifty9/poolmonitor/service/NotificationUtils.java
@@ -32,7 +32,7 @@ public class NotificationUtils {
             NotificationChannel mChannel = new NotificationChannel(
                     TRTL_NOTIFICATION_CHANNEL,
                     "Primary",
-                    NotificationManager.IMPORTANCE_HIGH);
+                    NotificationManager.IMPORTANCE_MIN);
             notificationManager.createNotificationChannel(mChannel);
         }
 
